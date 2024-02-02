d1be595c7b86623bbb12839f44c8fcfb1a80ab2e
janmatla <janne.m.lahti@student.jyu.fi>
2024-02-02 23:05:19 +0200

    added sidebar to the app

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,14 @@
 <script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
+import Sidebar from './components/SideBar.vue'
 </script>
 
 <template>
+  <div class="app">
+		<Sidebar />
+	</div>
+
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
