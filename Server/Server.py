94d0bd7ecd5387ff147a91055b81f60e4f9016f9
paeljojy <patrik.e.j.jylha@student.jyu.fi>
2024-02-11 19:00:29 +0200

    sqlite3 and mariadb dependencies

--- Server/Server.py
+++ Server/Server.py
@@ -1,9 +1,14 @@
 import flask
 from flask import request, jsonify
+import sqlite3
+import mariadb
 
 app = flask.Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+
 
