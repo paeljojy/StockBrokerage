fdf6169d8650baadacd801fe99ec37c27e1063fc
janmatla <janne.m.lahti@student.jyu.fi>
2024-02-11 19:39:30 +0200

    added sqlite template to flask app

--- Server/Server.py
+++ Server/Server.py
@@ -9,6 +9,13 @@ app = flask.Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-
-
-
+@app.route('/getdb', methods=['GET'])
+def getdb():
+    conn = sqlite3.connect('../Database/Main.db')
+    cursor = conn.execute("SELECT * FROM USERS")
+    list = []
+    for row in cursor:
+        list.append(row)
+    cursor.close()
+    conn.close()
+    return jsonify(list)
