5ac089f58f5332881a81ddff8147e2381ce5cf32
paeljojy <patrik.e.j.jylha@student.jyu.fi>
2024-02-12 19:15:27 +0200

    Server class

--- Server/Server.py
+++ Server/Server.py
@@ -2,6 +2,22 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import sqlite3
 import mariadb
+import datetime
+import time
+import requests
+
+class Server():
+    cachedData = None
+    cachedData = None
+    lastFetchTime = None
+
+    def init(self):
+        self.cachedData = None
+
+    def getCachedData(self):
+        return self.cachedData
+
+server = Server()
 
 app = Flask(__name__)
 CORS(app)
@@ -9,6 +25,30 @@ CORS(app)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+def resolveCachedData(server):
+    data = server.getCachedData()
+    server.currentTime = datetime.datetime.now()
+
+    # If we have cached data and it's less than an hour old, return it
+    # INFO: 3600 * 1000ms = 1 hour
+    if server.cachedData is not None and server.currentTime - (server.lastFetchTime) < 3600 * 1000:
+        return server.cachedData
+
+    # Otherwise, fetch new data from REST API
+    res = requests.get('https://api.marketdata.app/v1/stocks/quotes/AAPL/')
+    data = res.json()
+
+    # Update the cache and the fetch time
+    cachedData = data
+    server.lastFetchTime = server.currentTime
+
+    return data
+
+@app.route('/api/stocks/apple')
+def getStocks():
+    data = resolveCachedData(server)
+    return jsonify(data)
 
 @app.route('/getdb')
 def getdb():
@@ -20,3 +60,4 @@ def getdb():
     cursor.close()
     conn.close()
     return jsonify(list)
+
