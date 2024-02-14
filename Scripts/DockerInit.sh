0c8b19de8d30ea7a1df838493ce31d64b347208e
paeljojy <patrik.e.j.jylha@student.jyu.fi>
2024-02-14 18:20:29 +0200

    Moved docker compose file to root dir

--- Scripts/DockerInit.sh
+++ Scripts/DockerInit.sh
@@ -3,6 +3,7 @@
 pushd "$(dirname "$0")/.." > /dev/null
 
 # docker build . --file docker/Dockerfile --tag stock_brokerage:$(date +%s)
-docker build . --file docker/Dockerfile --tag stock_brokerage
+# docker build . --file docker/Dockerfile --tag stock_brokerage
+docker-compose -f docker-compose.yml build
 
 popd > /dev/null
--- Scripts/DockerRun.sh
+++ Scripts/DockerRun.sh
@@ -3,7 +3,9 @@
 pushd "$(dirname "$0")/.." > /dev/null
 
 # docker container kill stock_brokerage:latest
-docker run -p 5173:5173 stock_brokerage:latest
+# docker run -p 5173:5173 stock_brokerage:latest
+docker compose --file docker-compose.yml up
+
 
 popd > /dev/null
 
