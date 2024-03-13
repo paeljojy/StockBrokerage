55e935db2a244eb7345dd4ad725d3b1d44a4235e
janmatla <janne.m.lahti@student.jyu.fi>
2024-03-14 00:56:37 +0200

    hide buyer and seller info from trade list by default

--- src/views/StocksView.vue
+++ src/views/StocksView.vue
@@ -25,6 +25,7 @@ export default {
             // his Google account from the popup
             db: [],
             email: '',
+            isAdminLoggedIn: false,
             isUserLoggedIn: false,
             // TODO: could wrap user data in an object
             userFirstName: '',
@@ -314,20 +315,20 @@ export default {
             <h2>Trades made on this server</h2>
             <table class="Bids">
                 <tr>
-                    <th>Buyer</th>
-                    <th>Seller</th>
                     <th>Stock</th>
                     <th>Amount</th>
                     <th>Price</th>
                     <th>Date</th>
+                    <th v-if="isAdminLoggedIn">Buyer</th>
+                    <th v-if="isAdminLoggedIn">Seller</th>
                 </tr>
                 <tr v-for="(trade, index) in tradeDataList" :key="index">      
-                    <td>{{ trade[0] }}</td>
-                    <td>{{ trade[1] }}</td>
                     <td>Apple, Inc (AAPL)</td>
                     <td>{{ trade[3] }}</td>
                     <td>{{ trade[4] }}</td>
                     <td>{{ formateDate(trade[5]) }}</td>
+                    <td v-if="isAdminLoggedIn">{{ trade[0] }}</td>
+                    <td v-if="isAdminLoggedIn">{{ trade[1] }}</td>
                 </tr>
             </table>
             <button @click="requestTrades">Fetch server trades</button>
